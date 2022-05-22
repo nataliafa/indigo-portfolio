e5b5e307e1345c22bd3ab025e21a24f0706ad6da
Natalia Fomina <fominatasha@gmail.com>
2022-05-22 02:03:16 +0200

    Transform skills to use json

--- components/skills.tsx
+++ components/skills.tsx
@@ -10,55 +10,21 @@ import {
 } from "@chakra-ui/react";
 import { BiCodeAlt } from "react-icons/bi";
 import { HiMinusSm } from "react-icons/hi";
-import {
-  FaHtml5,
-  FaCss3Alt,
-  FaSass,
-  FaAngular,
-  FaReact,
-  FaJava,
-  FaGithub,
-  FaDocker,
-  FaAws,
-} from "react-icons/fa";
-
-const data = [
-  {
-    heading: "Java & Kotlin Ecosystem",
-    text: ["Spring Boot, Spring Data, Spring Security", "Gradle, Maven"],
-  },
-  {
-    heading: "React, JavaScript and Single Page Applications",
-    text: [
-      "Experience in creating applications using Vanilla JS, ES6, React (Functional and Class Components, Hooks), JSX, React Router, Virtual DOM",
-      "AJAX, Fetch, NPM manager and WebSockets",
-    ],
-  },
-  {
-    heading: "Cloud Computing",
-    text: ["Google Cloud Platform (GCP) and Firebase", "Amazovn AWS"],
-  },
-  {
-    heading: "Cloud Computing",
-    text: [
-      "Google Cloud Platform (GCP) and Firebase",
-      "Amazovn AWS",
-      "Microsoft Azure",
-      "Oracle Cloud",
-    ],
-  },
-];
+import data from "./skills.json";
+import Icon from "./icon";
 
 const Skills = () => {
   return (
     <Center bg="blue.900">
       <VStack spacing={8} py={20} px={8} maxW="1400px">
         <Heading as="h2" alignSelf="flex-start" color="gray.100">
-          Professional Skills<chakra.span color="cyan.500">.</chakra.span>
+          {data.heading}
+          <chakra.span color="cyan.500">.</chakra.span>
         </Heading>
         <Grid templateColumns="1fr 1fr" gap={6}>
-          {data.map((item) => (
+          {data.cards.map((item, index) => (
             <VStack
+              key={index}
               align="left"
               spacing={4}
               p={8}
@@ -72,8 +38,8 @@ const Skills = () => {
                 </Heading>
               </HStack>
               <VStack align="left" spacing={1} color="gray.400">
-                {item.text.map((item) => (
-                  <Box pos="relative">
+                {item.text.map((item, index) => (
+                  <Box key={index} pos="relative">
                     <Box pos="absolute" top={1}>
                       <HiMinusSm fontSize="14px" />
                     </Box>
@@ -87,42 +53,12 @@ const Skills = () => {
           ))}
         </Grid>
         <HStack color="gray.400" spacing={7} pt={6}>
-          <VStack>
-            <FaHtml5 fontSize="70px" />
-            <Text fontWeight="bold">HTML5</Text>
-          </VStack>
-          <VStack>
-            <FaCss3Alt fontSize="70px" />
-            <Text fontWeight="bold">CSS3</Text>
-          </VStack>
-          <VStack>
-            <FaSass fontSize="70px" />
-            <Text fontWeight="bold">Sass</Text>
-          </VStack>
-          <VStack>
-            <FaAngular fontSize="70px" />
-            <Text fontWeight="bold">Angular</Text>
-          </VStack>
-          <VStack>
-            <FaReact fontSize="70px" />
-            <Text fontWeight="bold">React</Text>
-          </VStack>
-          <VStack>
-            <FaJava fontSize="70px" />
-            <Text fontWeight="bold">Java</Text>
-          </VStack>
-          <VStack>
-            <FaGithub fontSize="70px" />
-            <Text fontWeight="bold">Git</Text>
-          </VStack>
-          <VStack>
-            <FaDocker fontSize="70px" />
-            <Text fontWeight="bold">Docker</Text>
-          </VStack>
-          <VStack>
-            <FaAws fontSize="70px" />
-            <Text fontWeight="bold">AWS</Text>
-          </VStack>
+          {data.icons.map((item, index) => (
+            <VStack key={index}>
+              <Icon name={item.iconName} fontSize="70px" />
+              <Text fontWeight="bold">{item.text}</Text>
+            </VStack>
+          ))}
         </HStack>
       </VStack>
     </Center>
