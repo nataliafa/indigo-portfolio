0bb4203af0debb72ab3e219398da74e1c8f8be8a
Natalia Fomina <fominatasha@gmail.com>
2022-06-17 01:52:33 +0200

    Make responsive

--- components/skills.tsx
+++ components/skills.tsx
@@ -8,65 +8,59 @@ import {
   Stack,
   Text,
   Container,
+  Flex,
 } from "@chakra-ui/react";
-import { BiCodeAlt } from "react-icons/bi";
-import { HiMinusSm } from "react-icons/hi";
 import data from "./skills.json";
 import Icon from "./icon";
+import Dot from "./dot";
 
-const Skills = () => {
-  return (
-    <Center bg="blue.900">
-      <Container maxW="1400px" py={20} px={8}>
-        <VStack spacing={8}>
-          <Heading alignSelf="self-start">
-            {data.heading}
-            <Text as="span" color="cyan.500">
-              .
-            </Text>
-          </Heading>
-          <Grid templateColumns="1fr 1fr" gap={6}>
-            {data.cards.map((item, index) => (
-              <Stack
-                key={index}
-                spacing={4}
-                p={8}
-                bg="blue.800"
-                borderRadius={8}
-              >
-                <HStack color="gray.100">
-                  <BiCodeAlt fontSize="22px" />
-                  <Heading as="h3" size="sm">
-                    {item.heading}
-                  </Heading>
-                </HStack>
-                <Stack spacing={1} color="gray.400">
-                  {item.text.map((item, index) => (
-                    <Box key={index} pos="relative">
-                      <Box pos="absolute" top={1}>
-                        <HiMinusSm fontSize="14px" />
-                      </Box>
-                      <Text fontSize="sm" ml={5}>
-                        {item}
-                      </Text>
+const Skills = () => (
+  <Center bg="blue.900">
+    <Container maxW="1400px" py={{ base: 8, lg: 20 }} px={{ base: 5, lg: 8 }}>
+      <Stack spacing={8}>
+        <Heading>
+          {data.heading}
+          <Dot />
+        </Heading>
+        <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} gap={6}>
+          {data.cards.map((item, index) => (
+            <Stack
+              key={index}
+              spacing={4}
+              p={{ base: 4, md: 8 }}
+              bg="blue.800"
+              borderRadius={8}
+            >
+              <HStack color="gray.100">
+                <Icon name="BiCodeAlt" fontSize="22px" />
+                <Heading as="h3" size="sm">
+                  {item.heading}
+                </Heading>
+              </HStack>
+              <Stack spacing={1} color="gray.400">
+                {item.text.map((item, index) => (
+                  <Box key={index} pos="relative">
+                    <Box pos="absolute" top={1}>
+                      <Icon name="HiMinusSm" fontSize="14px" />
                     </Box>
-                  ))}
-                </Stack>
+                    <Text ml={5}>{item}</Text>
+                  </Box>
+                ))}
               </Stack>
-            ))}
-          </Grid>
-          <HStack color="gray.400" spacing={7} pt={6}>
-            {data.icons.map((item, index) => (
-              <VStack key={index}>
-                <Icon name={item.iconName} fontSize="70px" />
-                <Text fontWeight="bold">{item.text}</Text>
-              </VStack>
-            ))}
-          </HStack>
-        </VStack>
-      </Container>
-    </Center>
-  );
-};
+            </Stack>
+          ))}
+        </Grid>
+        <Flex flexWrap="wrap" justifyContent="center" gap={6} color="gray.400">
+          {data.icons.map((item, index) => (
+            <VStack key={index} w="90px">
+              <Icon name={item.iconName} fontSize="70px" />
+              <Text fontWeight="bold">{item.text}</Text>
+            </VStack>
+          ))}
+        </Flex>
+      </Stack>
+    </Container>
+  </Center>
+);
 
 export default Skills;
