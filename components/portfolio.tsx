a4557edced0e839f9409f26b94e9363bf48d07b3
Natalia Fomina <fominatasha@gmail.com>
2022-06-17 01:59:00 +0200

    Make responsive

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -8,57 +8,57 @@ import {
   Button,
   Container,
 } from "@chakra-ui/react";
-import { HiOutlineExternalLink } from "react-icons/hi";
 import data from "./portfolio.json";
+import Icon from "./icon";
 import Dot from "./dot";
 
-const Portfolio = () => {
-  return (
-    <Center bg="blue.800">
-      <Container maxW="1400px" py={20} px={8}>
-        <Stack spacing={8}>
-          <Heading>
-            {data.heading}
-            <Dot />
-          </Heading>
-          <Text color="gray.400" align="justify">
-            {data.text}
-          </Text>
-          <Grid templateColumns="1fr 1fr 1fr" gap={16}>
-            {data.cards.map((item, index) => (
-              <Stack key={index} pos="relative" spacing={5} pb={16}>
-                <Image
-                  src={item.image}
-                  alt={item.imageAlt}
-                  style={{ borderRadius: "8px" }}
-                  width={700}
-                  height={400}
-                />
-                <Heading as="h3" size="sm">
-                  {item.heading}
-                </Heading>
-                <Text fontSize="sm" color="gray.400" align="justify">
-                  {item.text}
-                </Text>
-                <Button
-                  as="a"
-                  href={item.link}
-                  target="_blank"
-                  pos="absolute"
-                  bottom={0}
-                  left={0}
-                  width="50%"
-                  rightIcon={<HiOutlineExternalLink />}
-                >
-                  Open project
-                </Button>
-              </Stack>
-            ))}
-          </Grid>
-        </Stack>
-      </Container>
-    </Center>
-  );
-};
+const Portfolio = () => (
+  <Center bg="blue.800">
+    <Container maxW="1400px" py={{ base: 8, lg: 20 }} px={{ base: 5, lg: 8 }}>
+      <Stack spacing={8}>
+        <Heading>
+          {data.heading}
+          <Dot />
+        </Heading>
+        <Text color="gray.400" align="justify">
+          {data.text}
+        </Text>
+        <Grid
+          templateColumns={{ base: "1fr", md: "1fr 1fr", lg: "1fr 1fr 1fr" }}
+          gap={16}
+        >
+          {data.cards.map((item, index) => (
+            <Stack key={index} pos="relative" spacing={5} pb={16}>
+              <Image
+                src={item.image}
+                alt={item.imageAlt}
+                style={{ borderRadius: "8px" }}
+                width={700}
+                height={400}
+              />
+              <Heading as="h3" size="sm">
+                {item.heading}
+              </Heading>
+              <Text align="justify" color="gray.400">
+                {item.text}
+              </Text>
+              <Button
+                as="a"
+                href={item.link}
+                target="_blank"
+                pos="absolute"
+                bottom={0}
+                left={0}
+                rightIcon={<Icon name="HiOutlineExternalLink" />}
+              >
+                Open project
+              </Button>
+            </Stack>
+          ))}
+        </Grid>
+      </Stack>
+    </Container>
+  </Center>
+);
 
 export default Portfolio;
