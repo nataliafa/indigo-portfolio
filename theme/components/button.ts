f73650ba292f76d156ef6c1d7b49a4da70cd53cb
Natalia Fomina <fominatasha@gmail.com>
2022-06-17 01:24:04 +0200

    Add width to buttons

--- theme/components/button.ts
+++ theme/components/button.ts
@@ -1,6 +1,9 @@
 import type { ComponentStyleConfig } from "@chakra-ui/theme";
 
 const Button: ComponentStyleConfig = {
+  baseStyle: {
+    width: ["100%", null, null, "50%"],
+  },
   variants: {
     solid: ({ colorScheme }) => ({
       bg: `${colorScheme}.500`,
