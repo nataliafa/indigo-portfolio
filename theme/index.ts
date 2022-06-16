e0b997cc99aa9e731e631566a8e2be117190be0b
Natalia Fomina <fominatasha@gmail.com>
2022-06-17 01:24:20 +0200

    Add heading and breakpoints

--- theme/index.ts
+++ theme/index.ts
@@ -1,12 +1,16 @@
 import { extendTheme } from "@chakra-ui/react";
 import Button from "./components/button";
+import Heading from "./components/heading";
 import colors from "./colors";
+import breakpoints from "./breakpoints";
 
 const theme = extendTheme({
   components: {
     Button,
+    Heading,
   },
   colors,
+  breakpoints,
 });
 
 export default theme;
