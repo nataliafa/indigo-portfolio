d0dd630efeb62b843c94c2bcfaf0034f56319a14
Natalia Fomina <fominatasha@gmail.com>
2022-06-17 02:53:12 +0200

    Create container Chakra UI

---
+++ theme/components/container.ts
@@ -0,0 +1,18 @@
+import type { ComponentStyleConfig } from "@chakra-ui/theme";
+
+const Container: ComponentStyleConfig = {
+  variants: {
+    responsive: {
+      maxW: "1400px",
+      pt: { base: 8, lg: 20 },
+      pb: { base: 8, lg: 20 },
+      pl: { base: 5, lg: 8 },
+      pr: { base: 5, lg: 8 },
+    },
+  },
+  defaultProps: {
+    variant: "responsive",
+  },
+};
+
+export default Container;
--- theme/index.ts
+++ theme/index.ts
@@ -1,6 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 import Button from "./components/button";
 import Heading from "./components/heading";
+import Container from "./components/container";
 import colors from "./colors";
 import breakpoints from "./breakpoints";
 
@@ -8,6 +9,7 @@ const theme = extendTheme({
   components: {
     Button,
     Heading,
+    Container,
   },
   colors,
   breakpoints,
