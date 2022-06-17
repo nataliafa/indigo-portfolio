cb7c00221b2b89abd2f36ec2491fe4e7c88f48c4
Natalia Fomina <fominatasha@gmail.com>
2022-06-17 02:13:55 +0200

    Make responsive

--- components/education.tsx
+++ components/education.tsx
@@ -10,56 +10,51 @@ import {
   Box,
 } from "@chakra-ui/react";
 import data from "./education.json";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
 import Dot from "./dot";
 
-const Education = () => {
-  return (
-    <Center bg="blue.900">
-      <Container maxW="1400px" py={20} px={8}>
-        <Heading mb={8}>
-          {data.heading}
-          <Dot />
-        </Heading>
-        <Stack bg="blue.800" p={10} borderRadius={8} spacing={5}>
-          {data.items.map((item, index) => {
-            const isLast = index !== data.items.length - 1;
-            return (
-              <Box key={index}>
-                <Grid templateColumns="1fr 1fr" gap={6}>
-                  <Stack spacing={0.5}>
-                    <Text fontWeight="Bold" color="white">
-                      Stanford University
-                    </Text>
-                    <Link
-                      color="blue.600"
-                      display="flex"
-                      alignItems="center"
-                      href={item.url}
-                      isExternal
-                    >
-                      {item.url} <ExternalLinkIcon mx={1} />
-                    </Link>
-                    <Text color="gray.400">{item.location}</Text>
-                    <Text color="gray.400">{item.date}</Text>
-                  </Stack>
-                  <Stack spacing={0.5}>
-                    <Text fontWeight="Bold" color="white">
-                      Description
-                    </Text>
-                    <Text color="gray.400">{item.description}</Text>
-                  </Stack>
-                </Grid>
-                {isLast && (
-                  <Divider borderBottomWidth={3} borderBottomColor="blue.900" />
-                )}
-              </Box>
-            );
-          })}
-        </Stack>
-      </Container>
-    </Center>
-  );
-};
+const Education = () => (
+  <Center bg="blue.900">
+    <Container maxW="1400px" py={{ base: 8, lg: 20 }} px={{ base: 5, lg: 8 }}>
+      <Heading mb={8}>
+        {data.heading}
+        <Dot />
+      </Heading>
+      <Stack spacing={5} p={{ base: 5, md: 8 }} bg="blue.800" borderRadius={8}>
+        {data.items.map((item, index) => {
+          const isLast = index !== data.items.length - 1;
+          return (
+            <Box key={index}>
+              <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} gap={6}>
+                <Stack spacing={0.5}>
+                  <Text fontWeight="bold" color="white">
+                    Stanford University
+                  </Text>
+                  <Link color="blue.600" href={item.url} isExternal>
+                    {item.url}
+                  </Link>
+                  <Text color="gray.400">{item.location}</Text>
+                  <Text color="gray.400">{item.date}</Text>
+                </Stack>
+                <Stack spacing={0.5}>
+                  <Text fontWeight="bold" color="white">
+                    Description
+                  </Text>
+                  <Text color="gray.400">{item.description}</Text>
+                </Stack>
+              </Grid>
+              {isLast && (
+                <Divider
+                  mt={3}
+                  borderBottomWidth={3}
+                  borderBottomColor="blue.900"
+                />
+              )}
+            </Box>
+          );
+        })}
+      </Stack>
+    </Container>
+  </Center>
+);
 
 export default Education;
